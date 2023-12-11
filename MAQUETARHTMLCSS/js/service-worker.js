081aab928646b2750ce8e5097249482f6c81febc
Cristina_Colimba <cris_c1004@hotmail.com>
2023-12-10 19:35:34 -0500

    errores

--- MAQUETARHTMLCSS/js/service-worker.js
+++ MAQUETARHTMLCSS/js/service-worker.js
@@ -2,28 +2,27 @@
 
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
-'/',
-'/index.html',
-'/css/style.css',
-'/js/app.js',
-'/img/logo.png',
-'/img/img-1.jpg',
-'/img/img-2.jpg',
-'/img/img-3.jpg',
+    '/',
+    '/index.html',
+    '../css/style.css',  // Ruta relativa
+    '../Img/logo.png',   // Ruta relativa
+    '../Img/img-1.jpg',  // Ruta relativa
+    '../Img/img-2.jpg',  // Ruta relativa
+    '../Img/img-3.jpg',  // Ruta relativa
 ];
 
 self.addEventListener('install', (event) => {
-event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-    return cache.addAll(urlsToCache);
-    })
-);
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-event.respondWith(
-    caches.match(event.request).then((response) => {
-    return response || fetch(event.request);
-    })
-);
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
